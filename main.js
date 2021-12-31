f7c72ed3786d0f845aadef4ac0c573553f3be045
Michael Tang <tngmichael28@gmail.com>
2021-12-31 01:40:20 -0800

    Add coordinates to data model.

--- main.js
+++ main.js
@@ -1,23 +1,47 @@
 var data = {
-  currentDirection: 'east'
+  currentDirection: 'east',
+  engine: false,
+  coordinate: {
+    x: 0,
+    y: 0,
+  }
 }
+
+var engineInterval;
 
 var $car = document.getElementById('car');
 function turn(event) {
   if (event.key === 'ArrowUp' || event.key === 'w') {
-    $car.setAttribute('class', 'column-full rotate270')
+    $car.setAttribute('class', 'rotate270')
     data.currentDirection = 'north';
   } else if (event.key === 'ArrowRight' || event.key === 'd'){
-    $car.setAttribute('class', 'column-full')
+    $car.setAttribute('class', '')
     data.currentDirection = 'east';
   } else if (event.key === 'ArrowDown' || event.key === 's') {
-    $car.setAttribute('class', 'column-full rotate90')
+    $car.setAttribute('class', 'rotate90')
     data.currentDirection = 'south';
   } else if (event.key === 'ArrowLeft' || event.key === 'a') {
-    $car.setAttribute('class', 'column-full rotate180')
+    $car.setAttribute('class', 'rotate180')
     data.currentDirection = 'west';
   }
-  console.log(data);
 }
 
+// function engineSwitch(event) {
+//   if (event.key === ' ') {
+//     if (data.engine === false) {
+//       data.engine = true;
+//       engineInterval = setInterval(on, 16);
+//     }
+//   }
+// }
+
+// function on() {
+//   data.coordinate.x++;
+//   var x = data.coordinate.x;
+//   console.log(x)
+//   $car.style.left = (x * 10) + 'px';
+// }
+
 window.addEventListener('keydown', turn);
+// window.addEventListener('keydown', engineSwitch);
+// console.log();
